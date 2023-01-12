265c762cc70cfad4b1bf1e86c00b279a2c5e594c
Twin Edo <50164031+twinedo@users.noreply.github.com>
2023-01-12 13:19:51 +0700

    add predictionSlice and iosdir store

--- src/rdx/features/predictionSlice.js
+++ src/rdx/features/predictionSlice.js
@@ -1,20 +1,28 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  iosdir: '',
+	place: '',
+	legs: {},
+	coords: [{ latitude: 0, longitude: 0 }],
 };
 
-export const iosdirSlice = createSlice({
-  name: 'iosdir',
-  initialState,
-  reducers: {
-    setIosDir: (state, action) => {
-      state.iosdir === action.payload;
-    },
-  },
+export const predictionSlice = createSlice({
+	name: 'prediction',
+	initialState,
+	reducers: {
+		setCoords: (state, action) => {
+			state.coords === action.payload;
+		},
+		setLegss: (state, action) => {
+			state.legs === action.payload;
+		},
+		setPlace: (state, action) => {
+			state.place === action.payload;
+		},
+	},
 });
 
 // Action creators are generated for each case reducer function
-export const {setIosDir} = iosdirSlice.actions;
+export const { setCoords, setLegs, setPlace } = predictionSlice.actions;
 
-export default iosdirSlice.reducer;
+export default predictionSlice.reducer;
